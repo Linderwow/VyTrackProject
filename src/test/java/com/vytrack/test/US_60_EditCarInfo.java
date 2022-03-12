89617949a84207f749ce1c55b4273fa37ecaa6b0
Olesia Sokol <lesy0101@me.com>
2022-03-11 20:00:03 -0500

    US-60_working version with explicit wait

--- src/test/java/com/vytrack/test/US_60_EditCarInfo.java
+++ src/test/java/com/vytrack/test/US_60_EditCarInfo.java
@@ -2,13 +2,13 @@ package com.vytrack.test;
 
 import com.github.javafaker.Faker;
 import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
 import com.vytrack.utilities.VytrackUtils;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -27,19 +27,27 @@ public class US_60_EditCarInfo extends TestBase {
     @DataProvider(name = "US-60_Credentials")
     public Object[][] credentials() {
         return new Object[][]{{"usernameStoreManager1", "password"},
-                {"usernameTruckDrivers2", "password"},
-               {"usernameSalesManager1", "password"},
+                {"usernameTruckDrivers3", "password"},
+                {"usernameSalesManager1", "password"},
         };}
+
 
     @Test(dataProvider = "US-60_Credentials")
     public void edit_car_info_test(String username, String passwords) {
 
-        VytrackUtils.login(getProperty(username), getProperty(passwords)); //it's for login to homepage
-        BrowserUtils.sleep(2);
+        VytrackUtils.login(ConfigurationReader.getProperty(username), ConfigurationReader.getProperty(passwords)); //it's for login to homepage
+        //BrowserUtils.sleep(2);
+        //1. Find a Fleet tab and hover on it
 
         WebElement fleetTabs = Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Fleet' and contains(@class, 'title title-level-1')]"));
+        try{
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
+            wait.until(ExpectedConditions.visibilityOf(fleetTabs));
+        }catch(TimeoutException e){
+            e.printStackTrace();
+        }
         actions.moveToElement(fleetTabs).perform();
-        BrowserUtils.sleep(2);
+        //BrowserUtils.sleep(2);
 
         WebElement vehicles = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles']"));
         vehicles.click();
@@ -49,11 +57,14 @@ public class US_60_EditCarInfo extends TestBase {
 
         int num = faker.number().numberBetween(1, dots.size());
 
-        js.executeScript("arguments[0].scrollIntoView(true);", dots.get(num));
-        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        try{
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
+            wait.until(ExpectedConditions.visibilityOf(dots.get(num)));
+        }catch(TimeoutException e){
+            e.printStackTrace();
+        }
         actions.moveToElement(dots.get(num)).pause(1000).click(dots.get(num)).pause(2000).perform();
 
-        Driver.getDriver().manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
 
         WebElement deleteBtn = Driver.getDriver().findElement(By.xpath("//div[@class='dropdown']//li[3]//a"));
         WebElement editBtn = Driver.getDriver().findElement(By.xpath("//div[@class='dropdown']//li[2]//a"));
