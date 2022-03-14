e435c71c5fd66fa0c35484ed16e187b1a09a55e6
Tamerlan <thuseynov2017@gmail.com>
2022-03-14 15:53:12 -0400

    update

--- src/test/java/com/vytrack/test/TestBase.java
+++ src/test/java/com/vytrack/test/TestBase.java
@@ -1,11 +1,11 @@
 package com.vytrack.test;
-
 import com.vytrack.pages.LoginPage;
 import com.vytrack.pages.TopMenu;
 import com.vytrack.utilities.Driver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import static com.vytrack.utilities.ConfigurationReader.getProperty;
 import static com.vytrack.utilities.Driver.getDriver;
 
@@ -13,11 +13,17 @@ public class TestBase {
 
     public static LoginPage LoginPage;
     public static TopMenu TopMenu;
+    public static WebDriverWait webDriverWait;
+    public static Actions action;
+
     @BeforeMethod
     public void setUp(){
         getDriver().get(getProperty("env"));
         LoginPage=new LoginPage();
         TopMenu=new TopMenu();
+        webDriverWait=new WebDriverWait(getDriver(),10);
+        action=new Actions(getDriver());
+
 
     }
     @AfterMethod
