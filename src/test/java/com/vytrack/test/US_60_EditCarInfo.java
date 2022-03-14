f2f13c4c5a863cdd4485ec71ac1cefc0ac122ac0
Olesia Sokol <lesy0101@me.com>
2022-03-14 18:47:24 -0400

    US-60_working version with POM

--- src/test/java/com/vytrack/test/US_60_EditCarInfo.java
+++ src/test/java/com/vytrack/test/US_60_EditCarInfo.java
@@ -1,6 +1,7 @@
 package com.vytrack.test;
 
 import com.github.javafaker.Faker;
+import com.vytrack.pages.Vehicle;
 import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
@@ -11,67 +12,65 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 public class US_60_EditCarInfo extends TestBase {
+    Vehicle vehicle;
 
     @DataProvider(name = "US-60_Credentials")
     public Object[][] credentials() {
-        return new Object[][]{{"usernameStoreManager1", "password"},
-                {"usernameTruckDrivers2", "password"},
+        return new Object[][]{{"usernameStoreManager4", "password"},
+                {"usernameTruckDrivers3", "password"},
                 {"usernameSalesManager1", "password"},
-        };}
+        };
+    }
 
 
     @Test(dataProvider = "US-60_Credentials")
     public void edit_car_info_test(String username, String passwords) {
 
+        vehicle = new Vehicle();
         VyTrackUtils.login(ConfigurationReader.getProperty(username), ConfigurationReader.getProperty(passwords)); //it's for login to homepage
-        //BrowserUtils.sleep(2);
-        //1. Find a Fleet tab and hover on it
 
-        WebElement fleetTabs = Driver.getDriver().findElement(By.xpath("//span[normalize-space()='Fleet' and contains(@class, 'title title-level-1')]"));
-        try{
+        actions.moveToElement(TopMenu.fleetLink).perform();
+
+        try {
             WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
-            wait.until(ExpectedConditions.visibilityOf(fleetTabs));
-        }catch(TimeoutException e){
+            wait.until(ExpectedConditions.visibilityOf(TopMenu.fleetLink));
+        } catch (TimeoutException e) {
             e.printStackTrace();
         }
-        actions.moveToElement(fleetTabs).perform();
-        //BrowserUtils.sleep(2);
 
-        WebElement vehicles = Driver.getDriver().findElement(By.xpath("//span[.='Vehicles']"));
-        vehicles.click();
-        BrowserUtils.sleep(2);
+        TopMenu.fleetSub_Vehicles.click();
 
-        List<WebElement> dots = Driver.getDriver().findElements(By.xpath("//a[.='...']"));
+        int num = faker.number().numberBetween(1, vehicle.allOfDots.size());
 
-        int num = faker.number().numberBetween(1, dots.size());
+        //js.executeScript("arguments[0].scrollIntoView(true);", vehicle.allOfDots.get(num));
 
-        try{
+        try {
             WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
-            wait.until(ExpectedConditions.visibilityOf(dots.get(num)));
-        }catch(TimeoutException e){
+            wait.until(ExpectedConditions.visibilityOf(vehicle.allOfDots.get(num)));
+        } catch (TimeoutException e) {
             e.printStackTrace();
         }
-        actions.moveToElement(dots.get(num)).pause(1000).click(dots.get(num)).pause(2000).perform();
 
+        actions.moveToElement(vehicle.allOfDots.get(num)).pause(1000).click(vehicle.allOfDots.get(num)).pause(2000).perform();
 
-        WebElement deleteBtn = Driver.getDriver().findElement(By.xpath("//div[@class='dropdown']//li[3]//a"));
-        WebElement editBtn = Driver.getDriver().findElement(By.xpath("//div[@class='dropdown']//li[2]//a"));
-        WebElement viewBtn = Driver.getDriver().findElement(By.xpath("//div[@class='dropdown']//li[1]//a"));
+        String delete = vehicle.deleteButton.getAttribute("title");
+        String edit = vehicle.editButton.getAttribute("title");
+        String view = vehicle.viewButton.getAttribute("title");
 
-        String delete = deleteBtn.getAttribute("title");
-        String edit = editBtn.getAttribute("title");
-        String view = viewBtn.getAttribute("title");
+        List<String> expectedIconOptions = new ArrayList<>(Arrays.asList("View", "Edit", "Delete"));
 
-        String expectedDelete = "Delete";
-        String expectedEdit = "Edit";
-        String expectedView = "View";
+        List<String> actualIconOptions = new ArrayList<>();
+        actualIconOptions.addAll(Arrays.asList(view, edit, delete));
 
-        Assert.assertEquals(delete, expectedDelete);
-        Assert.assertEquals(edit, expectedEdit);
-        Assert.assertEquals(view, expectedView);
+        assertEquals(actualIconOptions, expectedIconOptions);
 
     }
 }
