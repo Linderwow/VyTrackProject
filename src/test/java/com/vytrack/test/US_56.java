09f6543dd81a88ebc6f9cb3b763e4140745f387f
Hewan <Darmelowhewan@gmail.com>
2022-03-17 15:40:18 -0400

    Automation is Done

--- src/test/java/com/vytrack/test/US_56.java
+++ src/test/java/com/vytrack/test/US_56.java
@@ -1,5 +1,36 @@
 package com.vytrack.test;
 
-public class US_56 {
+import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VytrackUtils;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class US_56 extends TestBase {
+
+    @Test
+    public void verify_page() {
+
+        VytrackUtils.loginAsSalesManager();
+        Driver.getDriver().findElement(By.xpath("//i[@title='Get help']")).click();
+        Set<String> windowHandles = Driver.getDriver().getWindowHandles();
+        String currentTitle = "";
+        String expectedTitle = "Welcome to Oro Documentation";
+        Iterator var4 = windowHandles.iterator();
+
+        while(var4.hasNext()) {
+            String each = (String)var4.next();
+            Driver.getDriver().switchTo().window(each);
+            if (!Driver.getDriver().getCurrentUrl().contains("qa")) {
+                currentTitle = Driver.getDriver().getTitle();
+                break;
+            }
+        }
+
+        Assert.assertEquals(currentTitle, expectedTitle);
+    }
     //Project started
 }
